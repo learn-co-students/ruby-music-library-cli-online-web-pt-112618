db3121ef002a08d57bd95b6482ca71fe373e5834
Sam Bennrubi <sbennrubi@gmail.com>
2019-01-20 14:13:11 +0000

    Done.

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,6 +1,6 @@
 module Concerns::Findable
   def find_by_name(name)
-    all.detect{ |o| o.name == name }
+    all.detect{ |object| object.name == name }
   end
 
   def find_or_create_by_name(name)
